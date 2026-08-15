8793c6bdad447a63b00528e5cd62b61db532acb9
Kadir niğdelioğlu <194108137+knigdelioglu@users.noreply.github.com>
2026-08-16 00:32:34 +0300

    phase 18: fix Drift native connection type

--- lib/core/database/connection/native.dart
+++ lib/core/database/connection/native.dart
@@ -1,10 +1,11 @@
 import 'dart:io';
 
+import 'package:drift/drift.dart';
 import 'package:drift/native.dart';
 import 'package:path/path.dart' as p;
 import 'package:path_provider/path_provider.dart';
 
-Future<NativeDatabase> openNativeConnection() async {
+Future<QueryExecutor> openNativeConnection() async {
   final Directory directory = await getApplicationSupportDirectory();
   final Directory dbDirectory = Directory(p.join(directory.path, 'database'));
   await dbDirectory.create(recursive: true);
