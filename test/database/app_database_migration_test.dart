84ed5d6411bd47f249b8d11fab663d38cb4b177e
Kadir niğdelioğlu <194108137+knigdelioglu@users.noreply.github.com>
2026-08-16 10:03:38 +0300

    test(db): import Drift variable support

--- test/database/app_database_migration_test.dart
+++ test/database/app_database_migration_test.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:drift/drift.dart';
 import 'package:drift/native.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:not_app/core/database/app_database.dart';
