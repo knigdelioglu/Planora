90452cc3cde8d615041bfa34355cbb8ed39d33d5
Kadir niğdelioğlu <194108137+knigdelioglu@users.noreply.github.com>
2026-08-16 10:20:57 +0300

    fix(android): enable notification core library desugaring

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -20,6 +20,7 @@ android {
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
+        isCoreLibraryDesugaringEnabled = true
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
@@ -30,6 +31,7 @@ android {
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
+        multiDexEnabled = true
     }
 
     signingConfigs {
@@ -50,6 +52,10 @@ android {
             isShrinkResources = false
         }
     }
+}
+
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 }
 
 kotlin {
