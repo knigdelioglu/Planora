e68498faee10e99caa6f711c86012bd4c5e3023b
Kadir niğdelioğlu <194108137+knigdelioglu@users.noreply.github.com>
2026-08-17 20:40:24 +0300

    add linked note entity

---
+++ lib/features/notes/domain/entities/linked_note.dart
@@ -0,0 +1,11 @@
+final class LinkedNoteEntity {
+  const LinkedNoteEntity({
+    required this.noteId,
+    required this.cardId,
+    required this.title,
+  });
+
+  final String noteId;
+  final String cardId;
+  final String title;
+}
