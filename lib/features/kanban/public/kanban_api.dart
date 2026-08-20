b203e7aada8c2015ed05ae55ec07c01e51f708af
Kadir niğdelioğlu <194108137+knigdelioglu@users.noreply.github.com>
2026-08-20 10:04:48 +0300

    refactor(kanban): expose columns in public API

--- lib/features/kanban/public/kanban_api.dart
+++ lib/features/kanban/public/kanban_api.dart
@@ -1,3 +1,4 @@
 export 'package:not_app/features/kanban/domain/entities/board.dart';
+export 'package:not_app/features/kanban/domain/entities/board_column.dart';
 export 'package:not_app/features/kanban/domain/entities/kanban_snapshot.dart';
 export 'package:not_app/features/kanban/domain/repositories/kanban_repository.dart';
