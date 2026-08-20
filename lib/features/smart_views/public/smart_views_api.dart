7b8c7b7d312acd250a43e7b7f0482c0d61d63b5e
Kadir niğdelioğlu <194108137+knigdelioglu@users.noreply.github.com>
2026-08-20 09:57:30 +0300

    feat(smart-views): expose public API

---
+++ lib/features/smart_views/public/smart_views_api.dart
@@ -0,0 +1,4 @@
+export 'package:not_app/features/smart_views/domain/entities/content_filter.dart';
+export 'package:not_app/features/smart_views/domain/entities/smart_view.dart';
+export 'package:not_app/features/smart_views/domain/entities/smart_view_result.dart';
+export 'package:not_app/features/smart_views/domain/repositories/smart_views_repository.dart';
